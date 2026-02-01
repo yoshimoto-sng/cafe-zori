0fcb21469a13f9e55db1b9386573e3d4e0fdfa37
yoshimoto-sng <yoshimoto@yamako-showten.jp>
2026-02-01 19:02:42 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+window.CAFEZORI_CONFIG = {
+  dai: [
+    { id: "red", label: "赤", image: "assets/dai/red.png" },
+    { id: "white", label: "白", image: "assets/dai/white.png" },
+    { id: "black", label: "黒", image: "assets/dai/black.png" }
+  ],
+  hanao: [
+    { id: "red", label: "赤", image: "assets/hanao/red.png" },
+    { id: "blue", label: "青", image: "assets/hanao/blue.png" },
+    { id: "green", label: "緑", image: "assets/hanao/green.png" },
+    { id: "white", label: "白", image: "assets/hanao/white.png" },
+    { id: "black", label: "黒", image: "assets/hanao/black.png" }
+  ]
+};
